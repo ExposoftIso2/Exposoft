9e204b07b6550f8bb1f0a84bfdda109311d69567
sergiograndemurillo@hotmail.com <sergiograndemurillo@hotmail.com>
2016-10-25 13:59:25 +0200

    Commit en el que incluimos el primer módulo en su versión 1.0.0 con la implementación de las multas proporcionada por los profesores.

---
+++ DGT/multas/src/main/java/DGT/multas/App.java
@@ -0,0 +1,13 @@
+package DGT.multas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
