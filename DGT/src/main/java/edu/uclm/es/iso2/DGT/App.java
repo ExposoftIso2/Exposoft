7ff895a685a70a57def5d7010fff2361f1430a44
sergiograndemurillo@hotmail.com <sergiograndemurillo@hotmail.com>
2016-10-25 13:27:51 +0200

    primer commit

---
+++ DGT/src/main/java/edu/uclm/es/iso2/DGT/App.java
@@ -0,0 +1,13 @@
+package edu.uclm.es.iso2.DGT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Ejemplomaven/src/main/java/edu/uclm/es/iso2/Ejemplomaven/App.java
@@ -0,0 +1,13 @@
+package edu.uclm.es.iso2.Ejemplomaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
