351ec10c28d45fb0a4eb63ebfa668ca857fb2767
Ferchudec <fernycozar@gmail.com>
2016-11-22 14:16:40 +0100

    Version 1.3

--- DGT/radar/src/main/java/edu/uclm/es/radar/Radar.java
+++ DGT/radar/src/main/java/edu/uclm/es/radar/Radar.java
@@ -1,14 +1,45 @@
 package edu.uclm.es.radar;
 
+import edu.uclm.esi.iso2.multas.domain.*;
+import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 public class Radar {
-	boolean encendido=false;
-	
-	public void encender(){
-		if(!encendido)
+
+	private boolean encendido = false;
+	private double velocidadMaxima;
+	private Date fecha;
+	private String localizacion;
+
+	public Radar(double vMax, String location){
+		velocidadMaxima=vMax;
+		localizacion=location;
+	}
+	private void encender() throws Exception{
+		if(!encendido){
 			encendido=true;
+			tomarFoto();
+		}else
+			throw new Exception("Ya encendido");
 	}
-	public void apagar(){
-		if(encendido)
+
+	private void apagar() throws Exception{
+		if(encendido){
 			encendido=false;
+		}else
+			throw new Exception("Ya apagado");
 	}
+
+	public void tomarFoto() {
+		Random r=new Random();
+		double velocidadCoche;
+		String matricula;
+		while(encendido){
+			velocidadCoche=r.nextDouble()*(velocidadMaxima+40);
+			matricula=r.nextInt(9999)+"ABC";
+			Inquiry(matricula, velocidadCoche, localizacion,velocidadMaxima);
+			TimeUnit.SECONDS.sleep(10);
+		}
+	}
+
 }
